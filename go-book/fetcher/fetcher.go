0f42a28f4358eb4f93205584651df86d8fceaec3
Lucas Araujo <luribeiro@schubergphilis.com>
2021-09-25 16:50:25 +0200

    sec1.5 ex

--- go-book/fetcher/fetcher.go
+++ go-book/fetcher/fetcher.go
@@ -2,27 +2,32 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+			url = "http://" + url
+		}
+
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
 
-		b, err := ioutil.ReadAll(resp.Body)
-		defer resp.Body.Close()
+		_, err = io.Copy(os.Stdout, resp.Body)
 
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s:% v\n", url, err)
 			os.Exit(1)
 		}
 
-		fmt.Printf("%s", b)
+		fmt.Println()
+		fmt.Printf("Status: %v", resp.Status)
 	}
 }
