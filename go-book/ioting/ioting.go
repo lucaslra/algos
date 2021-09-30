3214d23fd5ba5dd1e9acfbf15f12bfb83e8c5833
Lucas Araujo <luribeiro@schubergphilis.com>
2021-09-30 14:42:31 +0200

    fix pow ex 3.6.1

--- go-book/ioting/ioting.go
+++ go-book/ioting/ioting.go
@@ -5,14 +5,14 @@ import (
 )
 
 const (
-	KB float64 = 1000e0
-	MB float64 = 1000e1
-	GB float64 = 1000e2
-	TB float64 = 1000e3
-	PB float64 = 1000e4
-	EB float64 = 1000e5
-	ZB float64 = 1000e6
-	YB float64 = 1000e7
+	KB = 1e3
+	MB = 1e6
+	GB = 1e9
+	TB = 1e12
+	PB = 1e15
+	EB = 1e18
+	ZB = 1e21
+	YB = 1e24
 )
 
 func main() {
